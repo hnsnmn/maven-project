b507fa16d522d62f161d8bbce193fe73e39f1fb8
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-27 17:52:32 +0900

    web-project에서 구현되는 모든 controller는 BaseWebAppController를 상속받아야 한다.

---
+++ web-project/src/main/java/main/java/com/hnsnmn/webapp/controller/BaseWebAppController.java
@@ -0,0 +1,13 @@
+package main.java.com.hnsnmn.webapp.controller;
+
+import com.hnsnmn.spring.controller.BaseController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 4. 27.
+ * Time: 오후 5:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaseWebAppController extends BaseController {
+}
