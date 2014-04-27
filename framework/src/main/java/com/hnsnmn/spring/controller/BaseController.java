08acbe9a11f66a9779fcc8a3ba7afd3d1719ca90
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-27 17:53:30 +0900

    기본 Controller 구현

---
+++ framework/src/main/java/com/hnsnmn/spring/controller/BaseController.java
@@ -0,0 +1,22 @@
+package com.hnsnmn.spring.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 4. 25.
+ * Time: 오후 1:19
+ * To change this template use File | Settings | File Templates.
+ */
+@SuppressWarnings("unchecked")
+public class BaseController extends AbstractController {
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+}
