126aa048bac579976301eed132eb1d54245fe238
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-29 16:22:29 +0900

    View페이지 이동 URL을 관리하는 클래스

---
+++ web-project/src/main/java/com/hnsnmn/admin/webapp/common/View.java
@@ -0,0 +1,12 @@
+package com.hnsnmn.admin.webapp.common;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 4. 27.
+ * Time: 오후 6:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class View {
+	public static final String SIMPLE_LIST = "/simple/list";
+}
