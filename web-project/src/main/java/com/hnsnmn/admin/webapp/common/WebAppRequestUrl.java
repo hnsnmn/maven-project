2281997dd9e080279ebf762b06b4c514a6b039fe
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-29 16:23:08 +0900

    클라이언트에서 요청하는 URL을 관리하는 클래스

---
+++ web-project/src/main/java/com/hnsnmn/admin/webapp/common/WebAppRequestUrl.java
@@ -0,0 +1,12 @@
+package com.hnsnmn.admin.webapp.common;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 4. 27.
+ * Time: 오후 6:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class WebAppRequestUrl {
+	public static final String SIMPLE_LIST = "/simple/list";
+}
