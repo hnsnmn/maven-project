bc08987efb2e286b3ba9472b931935e43f7f8fd0
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-29 16:21:55 +0900

    ApiDispatcherServlet은 아직 구현하지 않았다.

---
+++ web-project/src/main/java/com/hnsnmn/admin/api/dispatcher/ApiDispatcherServlet.java
@@ -0,0 +1,13 @@
+package com.hnsnmn.admin.api.dispatcher;
+
+import com.hnsnmn.framework.dispatcher.BaseDispatcherServlet;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 4. 29.
+ * Time: 오전 9:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class ApiDispatcherServlet extends BaseDispatcherServlet {
+}
