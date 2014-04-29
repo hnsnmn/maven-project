e13c980107d014c9cc5df68538d83c60f7213533
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-29 16:23:45 +0900

    모든 웹컨트롤러가 상속을 받아야 하는 기본 Controller

---
+++ web-project/src/main/java/com/hnsnmn/admin/webapp/controller/BaseWebAppController.java
@@ -0,0 +1,18 @@
+package com.hnsnmn.admin.webapp.controller;
+
+import com.hnsnmn.framework.controller.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 4. 27.
+ * Time: 오후 5:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaseWebAppController extends BaseController {
+	@Autowired
+	HttpServletRequest request;
+}
